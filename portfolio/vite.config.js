d11ae96c13ef8dae664e0278b0b62f86403e5798
kithmal0015 <40-bit-0015@kdu.ac.lk>
2026-07-03 00:23:38 +0530

    Fixed vite base path for GitHub Pages

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/KT-Kodithuwakku/',
+  base: '/Portfolio/',  // මෙන්න මේක /Portfolio/ විදියට වෙනස් කරන්න
   plugins: [react(), 
     tailwindcss()
   ],
